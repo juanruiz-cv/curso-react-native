5d617fe44f5b272e602fa427600156fca9c65485
JuanLenovo <jruiz@travelpaq.com.ar>
2021-12-13 16:05:45 -0300

    listado de usuario

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -1,4 +1,41 @@
+import { useEffect, useState } from 'react';
+import { reqResApi } from '../api/reqRes';
+import { ReqResListado, Usuario } from '../interfaces/reqRes';
+
 export const Usuarios = () => {
+
+    const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+
+    useEffect(() => {
+        // Llamado al API
+        cargarUsuarios();
+    }, []);
+
+    const cargarUsuarios = async () => {
+        const response = await reqResApi.get<ReqResListado>('/users')
+            setUsuarios(response.data.data);
+    }
+
+    const renderItem = ({ id, first_name, last_name, email, avatar }: Usuario) => {
+        return (
+            <tr key={id.toString()}>
+                <td>
+                    <img
+                        src={avatar}
+                        alt={first_name}
+                        style={{
+                            width: 50,
+                            borderRadius: 100
+                        }}
+                    />
+                </td>
+                <td>{first_name} {last_name}</td>
+                <td>{email}</td>
+            </tr>
+        )
+
+    }
+
     return (
         <>
             <h3>Usuarios:</h3>
@@ -11,7 +48,9 @@ export const Usuarios = () => {
                     </tr>
                 </thead>
                 <tbody>
-
+                    {
+                        usuarios.map(usuario => renderItem(usuario))
+                    }
                 </tbody>
             </table>
         </>
